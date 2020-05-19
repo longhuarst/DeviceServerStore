267b5c8e7c6ec119a84007b46c4fac6460f6c568
coocx <841105197@qq.com>
2020-05-20 00:51:59 +0800

    从 rabbitmq 接收数据 存储到数据库 mysql 中

---
+++ src/main/java/com/lsxs/dss/repository/RawMessageSslRepository.java
@@ -0,0 +1,8 @@
+package com.lsxs.dss.repository;
+
+import com.lsxs.dss.entity.RawMessageSsl;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RawMessageSslRepository extends JpaRepository<RawMessageSsl, String> {
+
+}
